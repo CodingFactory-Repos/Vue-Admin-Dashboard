bf328955a24421d0eee317ead6d11505026b0e38
Eddy <eddy.muzaffar@outlook.com>
2022-09-22 12:03:05 +0200

    push function delete for resolve

--- src/store/product.js
+++ src/store/product.js
@@ -4,18 +4,28 @@ import axios from 'axios'
 export const useProductStore = defineStore('product', {
     state: () => {
         return{
-            products: []
+            products: [],
+            url : 'http://10.57.29.194:3000/products'
         }
 
     },
     actions: {
         async getAllProduct() {
             const fetchData = async () => {
-                const datas = await axios.get('http://10.57.29.194:3000/products');
+                const datas = await axios.get(this.url);
                 this.products = await datas.data;
 
             };
             await fetchData();
+            console.log(this.products)
+        },
+        deleteProduct(id){
+            id.forEach(res => axios.delete(this.url + '/'+ res))
+            id.forEach(res => console.log(this.url + '/' + res))
+            //await axios.delete(this.url + id);
+        },
+        AddProduct(data){
+            axios.post(this.url,data).then()
         }
     },
 })
--- src/views/Admin/AdminView.js
+++ src/views/Admin/AdminView.js
@@ -1,14 +1,49 @@
-import AdminSideBar from "@/components/AdminSideBar/AdminSideBarComponents";
+import axios from "axios";
+import {mapStores} from "pinia";
+import {useProductStore} from "@/store/product";
 
 export default ({
   name: 'AdminView',
-
-    components: {
-        AdminSideBar
-    },
-
     data() {
-        // Redirect to /admin/products route
-        this.$router.push('/admin/products');
+        return {
+            products: [],
+            selectProduct: []
+        }
+    },
+    computed: {
+        ...mapStores(useProductStore),
+
+    },
+    methods:{
+        chooseDeleteProduct (id, event){
+            console.log(this.products)
+            if(event.target.checked === true){
+                this.selectProduct.push(id)
+            }else{
+                const index = this.selectProduct.indexOf(id);
+                if (index > -1) { // only splice array when item is found
+                    this.selectProduct.splice(index, 1); // 2nd parameter means remove one item only
+                }
+            }
+            console.log(this.selectProduct)
+        },
+        deleteP(){
+            this.selectProduct.forEach((res) => {
+                const index = this.products.indexOf(res);
+
+                if (index > -1) { // only splice array when item is found
+                    this.products.splice(index, 1); // 2nd parameter means remove one item only
+                }
+            })
+
+            /*this.productStore.deleteProduct(this.selectProduct);*/
+        }
+    },
+    async mounted() {
+
+      await this.productStore.getAllProduct();
+        this.products = this.productStore.products
+
+
     },
 });
