ccd94c65ae45bbb44c4a496235da9d701730f730
Loule | Louis <35641311+Loule95450@users.noreply.github.com>
2022-09-22 13:55:29 +0200

    :dash: - Add a new pages un dashboard

--- src/views/Admin/AdminView.js
+++ src/views/Admin/AdminView.js
@@ -1,58 +1,14 @@
-import axios from "axios";
-import {mapStores} from "pinia";
-import {useProductStore} from "@/store/product";
-import {indexOf} from "core-js/internals/array-includes";
+import AdminSideBar from "@/components/AdminSideBar/AdminSideBarComponents";
 
 export default ({
   name: 'AdminView',
+
+    components: {
+        AdminSideBar
+    },
+
     data() {
-        return {
-            products: [],
-            selectProduct: [],
-
-        }
-    },
-    computed: {
-        ...mapStores(useProductStore),
-
-    },
-    methods:{
-        chooseDeleteProduct (id, event){
-            console.log(this.products)
-            if(event.target.checked === true){
-                this.selectProduct.push(id)
-            }else{
-                const index = this.selectProduct.indexOf(id);
-                if (index > -1) { // only splice array when item is found
-                    this.selectProduct.splice(index, 1); // 2nd parameter means remove one item only
-                }
-            }
-            console.log(this.selectProduct)
-        },
-        deleteP(){
-            for(let i = 0; i< this.selectProduct.length; i++){
-
-               this.products.forEach(object =>
-               {
-                   if(object.id === this.selectProduct[i]){
-                       let index = this.products.indexOf(object)
-                       this.products.splice(index, 1);
-
-                   }
-               })
-            }
-
-
-
-
-            /*this.productStore.deleteProduct(this.selectProduct);*/
-        }
-    },
-    async mounted() {
-
-      await this.productStore.getAllProduct();
-        this.products = this.productStore.products
-
-
+        // Redirect to /admin/products route
+        this.$router.push('/admin/products');
     },
 });
